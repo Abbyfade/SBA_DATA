6f3168480edba855507fd1f9767beba4059610ea
Abbyfade <abiolafadeyi10@gmail.com>
2023-07-13 13:35:18 +0100

    linked with postgres

--- app/db.py
+++ app/db.py
@@ -2,11 +2,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sbatest.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sbatest.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Abiola@localhost:5432/sba_data"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL#, connect_args = {"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
 
